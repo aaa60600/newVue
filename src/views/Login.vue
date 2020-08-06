d28f3fd65418809aa4fb1a85eef9bf58ecac8073
jerry_chou_B <jerry72225@msn.com>
2020-08-06 21:53:20 +0800

    修好了

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -22,7 +22,7 @@
 
 export default {
   name: 'Login',
-  data() {
+    data() {
        return {
          loginInit:{
            account:'',
--- src/views/NewAccount.vue
+++ src/views/NewAccount.vue
@@ -3,22 +3,46 @@
 
    <div class="newaccount">
      <label for="newaccount">建立帳號：</label>
-     <input name="newaccount" type="text" v-model="loginAccount.newaccount">
+     <input name="newaccount" type="text" v-model="accountData.newaccount">
      <label for="newpassword">密碼：</label>
-     <input name="newpassword" type="text" v-model="loginAccount.newpassword">
+     <input name="newpassword" type="text" v-model="accountData.newpassword">
      <label for="newemail">信箱：</label>
-     <input name="newemail" type="text" v-model="loginAccount.newemail">
+     <input name="newemail" type="text" v-model="accountData.newemail">
    </div>
     <div class="logaccount">
       <button class="logaccount" @click="resetInput">加入會員</button>
    </div>
-   <div v-if="loginData.account != ''&& loginData.password !=''">
-     <p>帳號 ： {{loginData.account}}</p>
-     <p>密碼 ： {{loginData.password}}</p>
+   <div v-if="accountData.account != ''&& accountData.password !=''">
+     <p>帳號 ： {{accountData.account}}</p>
+     <p>密碼 ： {{accountData.password}}</p>
    </div>
    </div>
 </template>
+<script>
 
+export default {
+  name: 'NewAccount',
+    data() {
+       return {
+         accountDataInit:{
+           newaccount:'',
+           newpassword:'',
+           newemail:'',
+         },
+         accountData:{}
+       }
+     },
+     created(){
+       this.resetInput();
+     },
+     methods:{
+    resetInput(){
+      this.accountData = JSON.parse(JSON.stringify(this.accountDataInit));
+    }
+  }
+}
+</script>
+</script>
 <style scoped>
 .NewAccount{
   background: url('../assets/account.jpg'); 
